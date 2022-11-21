4e211dcc0bc7c986cd2540a6878107da428e88c5
yonghowfung <yhfung940217@gmail.com>
2022-11-21 14:59:45 +0100

    sign up form updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :cds
 
-  validates :first_name, :last_name, :email, :address, presence: true
-  validates :email, uniqueness: true
-  validates :phone_number, numericality: { only_integer: true }
+  # validates :first_name, :last_name, :address, presence: true
+  validates :email, uniqueness: true, presence: true
+  # validates :phone_number, numericality: { only_integer: true }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
