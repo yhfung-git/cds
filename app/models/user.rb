bfc567980ce0d7123ba149d788b4aa244d44e70b
yonghowfung <yhfung940217@gmail.com>
2022-11-21 15:48:45 +0100

    validates updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :cds
 
-  # validates :first_name, :last_name, :address, presence: true
+  validates :first_name, :last_name, :address, presence: true
   validates :email, uniqueness: true, presence: true
-  # validates :phone_number, numericality: { only_integer: true }
+  validates :phone_number, numericality: { only_integer: true }, format: { with: /\d{9}/, message: "10 digits ONLY" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
