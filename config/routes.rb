4af58a974d6973331f9845382d1e445ba2e132ce
yonghowfung <yhfung940217@gmail.com>
2022-11-21 14:41:12 +0100

    updated model booking to datetime

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+
+# class ChangeColumnsToDate < ActiveRecord::Migration[7.0]
+#   def change
+#     change_column :bookings, :start_time, :datetime
+#     change_column :bookings, :end_time, :datetime
+#   end
+# end
--- db/migrate/20221121131324_change_columns_to_date.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsToDate < ActiveRecord::Migration[7.0]
-  def change
-    change_column :bookings, :start_time, :datetime
-    change_column :bookings, :end_time, :datetime
-  end
-end
