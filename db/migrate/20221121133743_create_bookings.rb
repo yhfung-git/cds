66729a371c9cda58d6dda38f984342cb3f367e29
yonghowfung <yhfung940217@gmail.com>
2022-11-22 10:50:35 +0100

    change datetime to date

--- db/migrate/20221121133743_create_bookings.rb
+++ db/migrate/20221121133743_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.datetime :start_time
-      t.datetime :end_time
+      t.date :start_time
+      t.date :end_time
       t.references :user, null: false, foreign_key: true
       t.references :cd, null: false, foreign_key: true
 
