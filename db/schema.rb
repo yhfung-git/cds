4660746c874c7cb1fe6d327165c1fc6e7a1deed9
d4v1dh <dhalley@null.net>
2022-11-24 17:16:29 +0100

    add category available and style for username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_160506) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_150636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_160506) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.date "start_time"
+    t.date "end_time"
     t.bigint "user_id", null: false
     t.bigint "cd_id", null: false
     t.datetime "created_at", null: false
@@ -60,6 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_160506) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.string "category"
+    t.boolean "available", default: true
     t.index ["user_id"], name: "index_cds_on_user_id"
   end
 
