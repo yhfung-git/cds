4059f1f4e1bf8d7cfaed2dc51ec2f4dc9581e825
Miarinkaja Lovalalaina <lovalalaina@gmail.com>
2022-11-24 11:25:45 +0100

    creating a div search

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -2,7 +2,14 @@ class Cd < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  include PgSearch::Model
 
   validates :name, :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_then: 1 }
+
+  pg_search_scope :search_by_name_and_description,
+  against: [ :name, :description ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
